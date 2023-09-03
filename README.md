8eb5f13c1efa7458cc7363d7da3b890c0ea58b3b
Sam <sami.grolleau@mailo.com>
2023-09-03 04:45:28 +0200

    Create .tasklist directory in user home, and print its content on screen

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tasklist
+
 A little GNU/Linux program to help to organize when too flexible
+
 Made with C++17 and ncurses
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,51 @@
 #include <cstdlib>
+#include <iostream>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <ncurses.h>
+#include <dirent.h>
+#include <fstream>
+#include <cstring>
 
-int main()
+int main(int argc, char* argv[])
 {
-	initscr();
+	// read program parameters
+	//TODO
 
-	const char* homedir;
-
-	if ((homedir = getenv("HOME")) == nullptr)
-	{
-		printw("ok\n");
+	// create .tasklist directory if not found
+	char* homedir = getenv("HOME");
+	if (homedir == nullptr)
 		homedir = getpwuid(getuid())->pw_dir;
+	strcat(homedir, "/.tasklist");
+
+	DIR* dir = opendir(homedir);
+	if (dir == nullptr)
+	{
+		if (mkdir(homedir, 0755) == 0)
+			dir = opendir(homedir);
+
+		if (dir == nullptr)
+		{
+			std::cerr << "Unable to open or create " << homedir << std::endl;
+			return -1;
+		}
 	}
 
-	printw("%s", homedir);
-	printw("\n");
+	// initialize ncurses if needed
+	initscr();
+	
+	while (struct dirent* dir_info = readdir(dir)) // foreach file in ~/.tasklist
+	{
+		if (dir_info->d_type == DT_REG) // if it's a regular file
+			printw("%s\n", dir_info->d_name); // print its name
+	}
 	refresh();
 
+	// close ncurses
+
+	closedir(dir);
 	getch();
 	endwin();
 
